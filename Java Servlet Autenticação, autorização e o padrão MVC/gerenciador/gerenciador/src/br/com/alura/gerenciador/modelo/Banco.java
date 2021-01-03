fce282ab5a9224b5aa6169aaf967a28bcdb782bb
Gabriel Romano <51065003+GabrielRomanoo@users.noreply.github.com>
2021-01-03 17:54:10 -0300

    Add files via upload

--- Java Servlet Autenticação, autorização e o padrão MVC/gerenciador/gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
+++ Java Servlet Autenticação, autorização e o padrão MVC/gerenciador/gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static List<Usuario> listaUsuarios = new ArrayList<Usuario>();
 	private static Integer chaveSequencial = 1;
 	
 	//bloco abaixo é executado quando a JVM carrega a classe
@@ -20,6 +21,15 @@ public class Banco {
 		empresa2.setNome("Caelum");
 		Banco.lista.add(empresa1);
 		Banco.lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("nico");
+		u1.setSenha("12345");
+		Usuario u2 = new Usuario();
+		u2.setLogin("ana");
+		u2.setSenha("12345");
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 	
 	public void adiciona(Empresa empresa) {
@@ -48,6 +58,15 @@ public class Banco {
 		Empresa empresa = this.buscaEmpresaPeloId(id);
 		empresa.setNome(nome);
 		empresa.setDataAbertura(dataAbertura);
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for (Usuario usuario : listaUsuarios) {
+			if (usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
+		return null;
 	}
 
 }
